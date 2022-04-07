b7de3765b8997214a61da338c9c7ec6515013365
SeungHyun <s20035@gsm.hs.kr>
2022-04-07 21:43:18 +0900

    UseCase 끝

---
+++ app/src/main/java/com/example/android_github_stars/domain/usecase/InsertFavoriteUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.android_github_stars.domain.usecase
+
+import com.example.android_github_stars.domain.FavoriteRepository
+import com.example.android_github_stars.domain.model.FavoriteItemModel
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+
+class InsertFavoriteUseCase(val repository: FavoriteRepository):UseCase {
+    operator fun invoke(itemModel: FavoriteItemModel): Completable {
+        return repository.insert(itemModel)
+    }
+}
